f065f5e82391feb22b81f1ecc4eb6cd641c86d73
Oscar Jara diaz <ojara@cognitiva.la>
2019-09-04 20:30:19 -0400

    [add] creation of script decoration with database connection

---
+++ library/decorator.py
@@ -0,0 +1,70 @@
+
+# -*- coding: utf-8 -*-
+
+import logging
+import functools
+import traceback
+
+import psycopg2.extras
+
+
+from config import postgres_connect
+
+def postgres_cursor(f):
+    def with_connection_(*args, **kwargs):
+        # or use a pool, or a factory function...
+        cnn = postgres_connect()
+        
+        try:
+            with cnn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+                cursor.itersize = 1000
+                rv = f(cursor, *args, **kwargs)
+
+        except Exception as e:
+            cnn.rollback()
+            err = f.__name__
+            err += ' ERROR: ' + str(e)
+            desc = str(err) + ", problema: " + str(e)
+            rv =  (
+                {
+                    'status':500,
+                    'msg':'Unexpected error',
+                    'error':desc
+                }
+            )
+        else:
+            cnn.commit() # or maybe not
+        finally:
+            cnn.close()
+        return rv
+    return with_connection_
+
+def postgres_cursor_connection(f):
+    def with_connection_(*args, **kwargs):
+        # or use a pool, or a factory function...
+        connection = postgres_connect()
+        
+        try:
+            with connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+                cursor.itersize = 1000
+                return f(cursor,connection,*args, **kwargs)
+
+        except Exception as e:
+
+            connection.rollback()
+            err = f.__name__
+            err += ' ERROR: ' + str(e)
+            desc = str(err) + ", problema: " + str(e)
+            return (
+                {
+                    'status':500,
+                    'msg':'Unexpected error',
+                    'error':desc
+
+                }
+            )
+        else:
+            connection.commit() # or maybe not
+        finally:
+            connection.close()
+    return with_connection_
