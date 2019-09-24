cd6e96789952766b7cc35abb018fa8243093db00
Oscar Jara diaz <ojara@cognitiva.la>
2019-09-24 16:12:03 -0300

    ADD: new view

--- view/view_user.py
+++ view/view_user.py
@@ -1,6 +1,9 @@
-from models.User    import User
+from models.user    import User
 from pbkdf2   import crypt
 from flask import request
+
+import base64
+
 
 def add_user_view(request):
     mail= request.json['email']
@@ -9,7 +12,6 @@ def add_user_view(request):
     second_last_name= request.json['apellido_m']
     id_rol= request.json['id_rol']
     id_unidad= request.json['id_unidad']
-    password= crypt(request.json['password'],'PORTAFOLIO',400)
     state= True if request.json['estado'] == 1 else False
     image=request.json['imagen']
     user = User(
@@ -19,30 +21,79 @@ def add_user_view(request):
         second_last_name=second_last_name,
         id_rol=id_rol,
         id_unidad=id_unidad,
-        password=password,
         state=state,
         image=image
     )
 
     return user.add_user()
 
+def recovery_pass_view(request):
+    mail = request.json['email']
 
-def list_user_view():
-    user = User()
-    return user.list_user()
+    user = User(
+        mail=mail
+    )
+
+    return user.recovery_pass()
+    
+def list_user_view(id,company):
+    user = User(
+        id=id
+    )
+    return user.list_user(company)
 
 def delete_user_view(request):
     
     id = request.json['id']
     user = User(id=id)
     return user.delete_user()
+
+def reset_password_view(request,token):
+    new_password = request.json['password']
+    acces_token = token
+
+
+    user = User(
+        password=new_password
+    )
+
+    return user.reset_password(acces_token)
+
 def login_view(request):
     mail = request.json['email']
     password = crypt(request.json['password'],'PORTAFOLIO',400)
-    print (password)
     user = User(
         mail=mail,
         password=password
     )
 
     return user.login()
+
+def validate_token_view(token):
+
+    return User().validate_token(token)
+
+def update_user_view(request):
+    mail = request.json['email']
+    name= request.json['nombre']
+    last_name= request.json['apellido_p']
+    second_last_name= request.json['apellido_m']
+    id_rol= request.json['id_rol']
+    id_unidad= request.json['id_unidad']
+    state= True if request.json['estado'] == 1 else False
+    image=request.json['imagen']
+    id = request.json['id']
+
+    user = User(
+        id=id,
+        mail=mail,
+        name=name,
+        last_name=last_name,
+        second_last_name=second_last_name,
+        id_rol=id_rol,
+        id_unidad=id_unidad,
+        state=state,
+        image=image
+    )
+
+    return user.update_user()
