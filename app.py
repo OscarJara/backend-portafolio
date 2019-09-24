b0f541005b19e2005530309f366aaf16d036460b
Oscar Jara diaz <ojara@cognitiva.la>
2019-09-24 16:09:35 -0300

    ADD: list user with parameters, user put, recovery and new password with acces-token

--- app.py
+++ app.py
@@ -31,11 +31,47 @@ def add_user():
 
 @app.route('/user',methods=['GET'])
 def get_user():
-    return jsonify(list_user_view())
+    id = request.args.get('id')
+    company = request.args.get('e')
+    return jsonify(list_user_view(id,company))
 
+@app.route('/user',methods=['PUT'])
+def update_user():
+    return jsonify(update_user_view(request))
 @app.route('/delete-user',methods=['POST'])
 def delete_user():
     return jsonify(delete_user_view(request))
+
+@app.route('/recovery-password',methods=['POST'])
+def recovery():
+    return jsonify(recovery_pass_view(request))
+
+@app.route('/new-password',methods=['POST'])
+def reset():
+    if 'Access-Token' not in request.headers:
+        return jsonify(
+            {
+                'status':401,
+                'msg':'autorización fallida',
+                'data':[],
+                'error':''
+            }
+        ),401
+    return jsonify(reset_password_view(request,request.headers['Acces-Token']))
+
+@app.route('/vd-tkn',methods=['GET'])
+def vd_token():
+    if 'Access-Token' not in request.headers:
+        return jsonify(
+            {
+                'status':401,
+                'msg':'autorización fallida',
+                'data':[],
+                'error':''
+            }
+        ),401
+    return jsonify(validate_token_view(request.headers['Acces-Token']))
+
 @app.route('/login',methods=['POST'])
 def login():
     return jsonify(login_view(request))
