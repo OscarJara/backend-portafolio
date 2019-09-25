875b2dffd57ee9bd81a047b8fd51ed35740b79e9
Oscar Jara diaz <ojara@cognitiva.la>
2019-09-25 20:03:49 -0300

    [add] view roles

---
+++ view/view_roles.py
@@ -0,0 +1,37 @@
+from models.roles import Role
+
+def roles_view(mail):
+    return Role().get_roles(mail)
+
+def add_role_view(request):
+    name = request.json['nombre']
+    description = request.json['descripcion']
+
+    rol = Role(
+        name=name,
+        description=description
+    )
+
+    return rol.add_role()
+
+def delete_role_view(request):
+    id = request.json['id']
+
+    rol = Role(
+        id=id
+    )
+
+    return rol.delete_role()
+
+def update_role_view(request):
+    id = request.json['id']
+    name = request.json['nombre']
+    description = request.json['descripcion']
+
+    rol = Role(
+        id=id,
+        name=name,
+        description=description
+    )
+
+    return rol.update_role()
